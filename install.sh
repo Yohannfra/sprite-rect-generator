5c741ac2729bbbc876e4bf7603395a3e73c306e1
Yohann <yohannassouline@hotmail.fr>
2020-11-05 07:08:07 +0100

    fix installation script

--- install.sh
+++ install.sh
@@ -5,13 +5,19 @@ if (( $EUID != 0 )); then
     exit 1
 fi
 
+
 set -e
+
+USER_CALLING=`logname`
+USER_HOME=`eval echo "~$USER_CALLING"`
+TARGET_DIRECTORY="$USER_HOME/.local/bin"
 
 python3 setup.py install
 
 echo -ne "#!/bin/bash\n\npython3 -m rect_generator \"\$@\"\n" > build/rect_generator
 chmod +x build/rect_generator
-mkdir -p ~/.local/bin
-cp build/rect_generator ~/.local/bin
+mkdir -p $TARGET_DIRECTORY
+cp build/rect_generator $TARGET_DIRECTORY
 
 echo "Done !"
+echo "The binary is in $TARGET_DIRECTORY"
